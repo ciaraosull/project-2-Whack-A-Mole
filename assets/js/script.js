fdd861c3c67a418e5d74c036000fa8c0d255482c
ciaraosull <ciaraosull@gmail.com>
2022-01-01 10:31:33 +0000

    Change js function comments to doc string format

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,31 @@
 console.log("test");
+//event listener DOM load before running game- code to be executed when page has finished loading
 
-//function peek for getting moles to pop up out of hole for certain amount of time and then disappear back into hole (maybe make a random time each time this happens?)
+//event listener added to buttons elements - code to be executed when user clicks buttons
 
-//function randomTimeBetweenPeek to calculate a random amount of time between the moles peeking up .  Used Math.round to keep the time as whole number?
+/**
+ * function peek for getting moles to pop up out of hole 
+ * for certain amount of time and then disappear back
+ * into hole (maybe make a random time each time this happens?)
+ */
 
-//function randomHoles to get random DOM element of “holes”.  Then to generate a random number (0-5) of the 6 holes (holes.lenght) the moles peek out of each time
+/**
+ * function randomTimeBetweenPeek to calculate
+ * a random amount of time between the moles peeking up.
+ * Used Math.round to keep the time as whole number?
+ */
 
-//function scoreboard to calculate the score increasing on every successful hit of mole
+/**
+ * function randomHoles to get random DOM element of “holes”.
+ * Then to generate a random number (0-5) of the
+ * 6 holes (holes.lenght) the moles peek out of each time
+ */
 
-//function timer & when timer is up stop game
+/**
+ * function scoreboard to calculate the
+ * score increasing on every successful hit of mole
+ */
+
+/**
+ * function timer & when timer is up stop game
+ */
