7d956bb33d5e91d2056e0f86f2aa6bdcccab6d4f
ciaraosull <ciaraosull@gmail.com>
2022-01-01 09:38:29 +0000

    Add draft comments to decribe all js functions needed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
 console.log("test");
+
+//function peek for getting moles to pop up out of hole for certain amount of time and then disappear back into hole (maybe make a random time each time this happens?)
+
+//function randomTimeBetweenPeek to calculate a random amount of time between the moles peeking up .  Used Math.round to keep the time as whole number?
+
+//function randomHoles to get random DOM element of “holes”.  Then to generate a random number (0-5) of the 6 holes (holes.lenght) the moles peek out of each time
+
+//function scoreboard to calculate the score increasing on every successful hit of mole
+
+//function timer & when timer is up stop game
