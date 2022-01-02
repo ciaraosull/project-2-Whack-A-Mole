d284464dcfd8ce85e40f42a28c1dab07e47098f8
ciaraosull <ciaraosull@gmail.com>
2022-01-02 22:32:36 +0000

    Add draft js to calculate randomHoles function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,38 @@
-console.log("test");
+
 //event listener DOM load before running game- code to be executed when page has finished loading
 
 //event listener added to buttons elements - code to be executed when user clicks buttons
+
+
+//function randomHoles to get random DOM element of “holes”. Then to generate a random number (0-2) of the 3 holes (holes.lenght) the moles peek out of each time//
+
+//try not make global variables - test only
+
+let hole = document.getElementsByClassName("hole");
+
+console.log(hole.length);
+
+let randomHole = Math.floor(Math.random() * hole.length);
+console.log(randomHole);
+
+
+
+ 
 
 /**
  * function peek for getting moles to pop up out of hole 
  * for certain amount of time and then disappear back
  * into hole (maybe make a random time each time this happens?)
  */
-
+ 
+ 
 /**
  * function randomTimeBetweenPeek to calculate
  * a random amount of time between the moles peeking up.
  * Used Math.round to keep the time as whole number?
  */
 
-/**
- * function randomHoles to get random DOM element of “holes”.
- * Then to generate a random number (0-5) of the
- * 6 holes (holes.lenght) the moles peek out of each time
- */
+
 
 /**
  * function scoreboard to calculate the
