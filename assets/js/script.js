8a811838ab1513554af8d825eebefefb570669fa
ciaraosull <ciaraosull@gmail.com>
2022-01-03 11:00:03 +0000

    Add function to generate randomTime between chosen holes

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,12 +7,12 @@
 //function randomHoles to get random DOM element of “holes”. 
 //Then to generate a random number (0-2) of the 3 holes (holes.lenght) the moles peek out of each time.
 
-let hole = document.getElementsByClassName("hole"); //try not make global variables - test only
+//let hole = document.getElementsByClassName("hole"); //try not make global variables - test only
 
-console.log(hole.length); //3
+//console.log(hole.length); //3
 
-let randomHole = Math.floor(Math.random() * hole.length);
-console.log(randomHole); //random number between 0-3
+//let randomHole = Math.floor(Math.random() * hole.length);
+//console.log(randomHole); //random number between 0-3
 
 //function randomHole(holes) {
 //    let x = Math.floor(Math.random() * hole.length);
@@ -20,21 +20,23 @@ console.log(randomHole); //random number between 0-3
 //    return hole;
 //   }
 
- 
-
-/**
- * function peek for getting moles to pop up out of hole (in css .classList.add("up")) 
- * for certain amount of time and then disappear back
- * into hole (maybe make a random time each time this happens?)
- */
- 
 /**
  * function randomTimeBetweenPeek to calculate
  * a random amount of time between the moles peeking up.
  * Used Math.round to keep the time as whole number?
  */
 
+function randomTime(min, max) {
+    return Math.random() * (max - min) + min;
+}
 
+console.log(randomTime(min, max));
+
+/**
+ * function peek for getting moles to pop up out of hole (in css .classList.add("up")) 
+ * for certain amount of time and then disappear back
+ * into hole (maybe make a random time each time this happens?)
+ */
 
 /**
  * function scoreboard to calculate the
